8b6e32bb4a14850c3c20f0909e7dba0dbcc6dc19
vincentvnoord <vannoord.vincent@gmail.com>
2024-02-03 16:28:08 +0100

    Adding and changing poll options

--- src/routes/CreatePoll.tsx
+++ src/routes/CreatePoll.tsx
@@ -1,18 +1,39 @@
 import React from "react";
 import { useState } from "react";
-
+import PollOption from "../components/PollOption";
 
 
 const NewPoll: React.FC = () => {
     const [options, setOptions] = useState<string[]>([]);
-    
+    const [newOption, setNewOption] = useState<string>("");
+
+    const newOptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const inputValue : string = e.target.value;
+        setNewOption(inputValue);
+    }
+
+    const addNewOption = () => {
+        if(newOption == ""){
+            console.log("New option can't be empty")
+            return;
+        }
+
+        const newOptions = [...options, newOption];
+        setOptions(newOptions);
+    }
+
     // Title, options inputs and create button
     return (
     <div className="create-poll">
         <input type="text" placeholder="Question/Title"/>
+        {
+            options.map((option, index) => (
+                <PollOption key={index} options={options} setOptions={setOptions} optionValue={option} index={index} />
+            ))
+        }
         <div>
-            <input type="text" placeholder="Option" />
-            <button>Add</button>
+            <input type="text" onChange={newOptionChange} placeholder="Option" />
+            <button  onClick={addNewOption}>Add</button>
         </div>
         <button>Create</button>
     </div>
